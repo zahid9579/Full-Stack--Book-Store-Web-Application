1a7b5272c257b814ae2b47d3448650fbfdce6a6e
Zahid-9579 <kamalzahid636@gmail.com>
2024-12-03 22:07:08 +0530

    this is for delivery info

--- testapp/admin.py
+++ testapp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Book, Cart, CartItem, Category
+from .models import Book, Cart, CartItem, Category, DeliveryInfo
 # Register your models here.
 
 admin.site.register(Book)
 admin.site.register(Cart)
 admin.site.register(CartItem)
 admin.site.register(Category)
+admin.site.register(DeliveryInfo)
